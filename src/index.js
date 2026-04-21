c4ec69510c26fd38242f57aa7d2531556552a5ec
Williams Hernández <hernandw@gmail.com>
2026-04-20 20:32:11 -0400

    :sparkles: creación de rutas

---
+++ src/index.js
@@ -0,0 +1,39 @@
+import express from 'express'
+import exphbs from 'express-handlebars'
+import appRouter from './routes/views.routes.js'
+import path from 'path'
+
+const app = express()
+const __dirname = path.resolve()
+
+const PORT = process.env.PORT || 4005
+
+//Middleware
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+//Static Files
+app.use(express.static(path.join(__dirname, "src/public")))
+
+
+// Configuración de handlebars
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "src/views"));
+
+app.engine(
+  "hbs",
+  exphbs.engine({
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "src/views/layouts"),
+    extname: ".hbs",
+  }),
+);
+
+//configuración de express-uploads
+
+//Rutas
+app.use('/', appRouter)
+
+app.listen(PORT, ()=>{
+    console.log(`🚀 Server Running on port http://localhost:${PORT}`)
+})
